ac919cd97725f3d20b14cd0c07a72e4be14865eb
Dmitrii <swarovskidima@gmail.com>
2025-08-30 22:39:28 +0700

    подключение пользователей из бд

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,7 +4,7 @@ from app.api import payment
 from app.api import server
 from app.api import key
 from app.api import payments_stars
-# from app.api import tg_webhook
+from app.api import tg_webhook
 from app.api import verify
 from app.api import jwt_auth
 
@@ -15,7 +15,7 @@ routers = (
     server.router,
     key.router,
     payments_stars.router,
-    # tg_webhook.router,
+    tg_webhook.router,
     verify.router,
     jwt_auth.router,
 )
